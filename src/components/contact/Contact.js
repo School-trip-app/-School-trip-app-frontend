c1551d0ffae2317e433ff0bd245245d698d07252
suhaibAln <201710390@stu.meu.edu.jo>
2022-12-11 19:07:22 +0000

    added a carsoul and a contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './contact.scss'
+import Navbar from '../navbar/Navbar';
+import Carousel from "./Carousel"
+import { ImHome } from "react-icons/im";
+
+
+const onSubmit = (e) => {
+  e.preventDefault()
+  const { name, email, message } = e.target.elements
+  let conFom = {
+    name: name.value,
+    email: email.value,
+    message: message.value,
+  }
+  console.log(conFom)
+}
+
 function Contact() {
+  const [formStatus, setFormStatus] = React.useState('Send')
+
   return (
-    <div>
+    <>
+      <Navbar />
+      <Carousel>
+        <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+      </Carousel>
+
+
+      <div className="container mt-5">
+      <h2 className="contact-title">Get in Touch</h2>
+      <div className='row '>
+
+        
+     
+      <form onSubmit={onSubmit}>
+        <div className="col-lg-8">
+            <div className='row'>
+
+            </div>
+          <textarea className="" id="message" required placeholder='Enter Message'  cols="30" rows="9"/>
+          <input className="" type="text" id="name" required placeholder='Enter your name' />
+          <input className="" type="email" id="email" required placeholder='Enter email address' />
+          <input className="" type="text" id="subject" required placeholder='Enter subject' />
+
+          
+        </div>    
+        <button className="button sbtbutton" type="submit">
+          {formStatus}
+        </button>
+      </form>
       
+
+
+
     </div>
+    </div>
+     
+    </>
   )
 }
 
