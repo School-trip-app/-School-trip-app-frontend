85403601a8b5d518488c962fd3475ca4ffb8707f
hamzahhisham306 <hamzahhisham306@gmail.com>
2022-12-11 15:23:02 +0000

    add footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import About from './components/aboutus/About';
 import Headern from './components/header/Headern';
 import Navbar from './components/navbar/Navbar';
 import Travel from './components/travel/Travel';
+import Video from './components/videotour/Video';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
      <Headern/>
      <Travel/>
      <About/>
+     <Video/>
+     <Footer/>
     </div>
   );
 }
