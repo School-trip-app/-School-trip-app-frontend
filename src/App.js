bd0eb285742c9abb64acfd723b6c294dec0c35e1
suhaibAln <201710390@stu.meu.edu.jo>
2022-12-13 18:54:28 +0000

    added a products page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import About from './pages/About/aboutus/About';
 import Headern from './components/header/Headern';
 import Travel from './components/travel/Travel';
 import Video from './components/videotour/Video';
+import Product from './pages/Products/Products';
+
 function App() {
   return (
     <div >
+      <Product />
       <Headern />
       <Travel />
       <About />
