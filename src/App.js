f4d7fef0aa4c4048adc109974dafb7fcfb7cff92
suhaibAln <201710390@stu.meu.edu.jo>
2022-12-14 16:41:14 +0000

    photographers

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import Headern from './components/header/Headern';
 import Travel from './components/travel/Travel';
 import Video from './components/videotour/Video';
 import Product from './pages/Products/Products';
-
+import Photographers from './pages/Photographers/Photographers';
 function App() {
   return (
     <div >
-      <Product />
+      <Photographers />
+      {/* <Product />
       <Headern />
       <Travel />
       <About />
-      <Video/>
+      <Video/> */}
     </div>
   );
 }
--- src/pages/Products/productCard.js
+++ src/pages/Products/productCard.js
@@ -13,22 +13,22 @@ function ProductCard(props) {
           <FaFireAlt className={"productCard_fastSelling"} />
 
           <div className='productCard_content'>
-                     <h3 className='productName'>{props.name}</h3>
-                     <div className='displayStack_1'>
-                         <div className='productPrice'>${props.price}</div>
-                         <div className='productSales'>{props.totalSales} units sold</div>
-                     </div>
-                     <div className='displayStack_2'>
-                         <div className='productRating'>
-                             {[...Array(props.rating)].map((index) => (
-                                 <FaStar id={index + 1 } key={index} />
-                             ))}
-                         </div>
-                         <div className='productTime'>{props.timeLeft} days left</div>
-                     </div>
-                 </div>
-             </div>
-         </div>
+            <h3 className='productName'>{props.name}</h3>
+            <div className='displayStack_1'>
+              <div className='productPrice'>${props.price}</div>
+              <div className='productSales'>{props.totalSales} units sold</div>
+            </div>
+            <div className='displayStack_2'>
+              <div className='productRating'>
+                {[...Array(props.rating)].map((index) => (
+                  <FaStar id={index + 1} key={index} />
+                ))}
+              </div>
+              <div className='productTime'>{props.timeLeft} days left</div>
+            </div>
+          </div>
+        </div>
+      </div>
 
     </>
   )
