e41d9d579fd3d9d72be0e910c50b38e5ca092d6b
Alhaimouni <m.alhaumouni@gmail.com>
2022-12-15 12:56:31 +0200

    all works

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Register from './components/register/Register';
 import TripDetails from './components/tripDetails/TripDetails';
+import Photographers from './pages/Photographers/Photographers';
+import Products from './pages/Products/Products';
 
 
 
@@ -35,6 +37,8 @@ root.render(
     <TripDetails title={'Package El-7ob'} city={'Amman'}
       description={'Mount Nebo is an elevated ridge located in Jordan, approximately 710 metres above sea level. Part of the Abarim mountain range,Mount Nebo is mentioned in the Bible as the place where Moses was granted a view of the Promised Land before his death. The view from thesummit provides a panorama of the West Bank across the Jordan River valley. The city of Jericho is usually visible from the summit,as is Al-quds on a very clear day. The biblical town of Nebo, now known as Khirbet al-Mukhayyat.'}
       date={'20-jun-2022'} people={30} meals={"lunch,dinner"} price={10} rate={4.5} startTime={'10.00am'} endTime={'5:00pm'} pickup={'School'} drop={'home'} images={[]} />
+    <Photographers />
+    <Products />
   </Router>
 );
 
---
+++ src/pages/Products/Products.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Products from './productCard'
+import './ProductCard.css'
+import contents from './content.js'; //fake data
+
+function Product() {
+  return (
+    <>
+      <section className='top-background'>
+        <p>Store </p>
+      </section>
+      <section className='packages'>
+        <div>
+          <span className='check'>Grab a special Souvenir to memorize your day</span>
+          <p className='upcome'> Souvenirs</p>
+        </div>
+      </section>
+      <div className='products'>
+        {contents.map(contents => (
+          <Products
+            key={contents.id}
+            image={contents.image}
+            name={contents.name}
+            price={contents.price}
+            totalSales={contents.totalSales}
+            timeLeft={contents.timeLeft}
+            rating={contents.rating}
+          />
+        ))}
+      </div>
+    </>
+  )
+
+}
+export default Product;
