4040b9cbf92d0b877cae017b2bf9e1e5eec0726e
Alhaimouni <m.alhaumouni@gmail.com>
2022-12-17 18:42:22 +0200

    edit font

--- src/pages/Photographers/Photographers.js
+++ src/pages/Photographers/Photographers.js
@@ -8,38 +8,38 @@ import Footer from '../../components/footer/Footer'
 function Photographers() {
   return (
     <>
-    <Navbar/>
-    <div>
-      <section className='top-background-photographer'>
-        <p>Photographers </p>
-      </section>
-      <section className='packages'>
-        <div>
-          <span className='check'>Photographers</span>
-          <p className='upcome'>Best way to keep memories   by photos of course idiots </p>
-        </div>
-      </section>
+      <Navbar />
       <div>
-        <section className='section-sb'>
-          <div className='container'>
-            <h3>Photographers</h3>
-            <div className='Cards'>
-              {PhotographersData.map((PhotographersData) => (
-                <div key={PhotographersData.id} className='Card'>
-                  <img src={PhotographersData.image} alt='Photographers' />
-                  <h3 className='h1-sb' >{PhotographersData.name}</h3>
-                  <p className=' icon '><FaEnvelope />{PhotographersData.email}</p>
-                  <p>{PhotographersData.phoneNumber}</p>
-                  <p><FaStar />{PhotographersData.rate}</p>
-                  <button className='btn-book-sb'>Book</button>
-                </div>
-              ))}
-            </div>
+        <section className='top-background-photographer'>
+          <p>Photographers </p>
+        </section>
+        <section className='packages'>
+          <div>
+            <span className='check'>Photographers</span>
+            <p className='upcome'>Best way to keep memories by photos </p>
           </div>
         </section>
+        <div>
+          <section className='section-sb'>
+            <div className='container'>
+              <h3>Photographers</h3>
+              <div className='Cards'>
+                {PhotographersData.map((PhotographersData) => (
+                  <div key={PhotographersData.id} className='Card'>
+                    <img src={PhotographersData.image} alt='Photographers' />
+                    <h3 className='h1-sb' >{PhotographersData.name}</h3>
+                    <p className=' icon '><FaEnvelope />{PhotographersData.email}</p>
+                    <p>{PhotographersData.phoneNumber}</p>
+                    <p><FaStar />{PhotographersData.rate}</p>
+                    <button className='btn-book-sb'>Book</button>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
-    </div>
-    <Footer/>
+      <Footer />
     </>
   );
 }
